e4c298521b606c78658d32af9a30900a4b751bdb
shankha013k267 <shankhadeepg@iisc.ac.in>
2025-05-01 13:27:13 +0530

    events.css updated

--- js/events.js
+++ js/events.js
@@ -125,22 +125,22 @@
 // Array of objects containing formatted text, page link and image to be displayed on the title page
 let major_events = [
   {
-    text: `<span style="font-size: 35px;"><b>Kyle Daigle</b></span> <br/><span style="font-size: 25px;"><b>COO, GitHub</b></span> <br/> <span style="font-size: 25px">visits IISc</span>`,
+    text: `<span class="text-size-35"><b>Kyle Daigle</b></span> <br/><span class="text-size-25"><b>COO, GitHub</b><br/>visits IISc</span>`,
     link: "",
     img: "./img/github.jpg"
   },
   {
-    text: `<span style="font-size: 70px;"><b>IISc</b></span> <br/> <span style="font-size: 30px;">OPEN DAY <br/> 2025</span>`,
+    text: `<span class="text-size-70"><b>IISc</b></span> <br/> <span class="text-size-30">OPEN DAY <br/> 2025</span>`,
     link: "./pages/open-day-2025",
     img: "./img/banner.webp"
   },
   {
-    text: `<span style="font-size: 35px;"><b>Algorithm <br/> Festival</b></span>`,
+    text: `<span class="text-size-35"><b>Algorithm <br/> Festival</b></span>`,
     link: "./pages/algorithms",
     img: "./img/algorithms/group-pic.webp"
   },
   {
-    text: `<span style="font-size: 30px;">DATABASED</span> <br/> <span style="font-size: 30px;"><b>TECH <br/> IDEATHON</b></span>`,
+    text: `<span class="text-size-30">DATABASED<br/><b>TECH<br/>IDEATHON</b></span>`,
     link: "./pages/ideathon",
     img: "./img/ideathon/ideathon_presentation.webp"
   }
